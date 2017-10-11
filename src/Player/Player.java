65fef4d8bac8543b8f5d91f7f6d6c2bb17a905fb
Ivan Ke <21key47@mybhs.org>
2017-10-11 16:43:39 -0700

    Added isCardInHand method, choseCard method, Commented  favor (for main)

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -3,9 +3,7 @@ package Player;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-/*make each plaer's hand separate. 
- * Construct 4 separate hands. 
- * Distinguish between the player who is currently playing.
+/*
  *  Is card in hand method
  */
 public class Player {
@@ -15,6 +13,7 @@ public class Player {
 	public int turns = 1;
 	public ArrayList<Card> hand;
 	public boolean hasCard;
+	
 	public Player() {
 		hand = new ArrayList<Card>();
 	}
@@ -24,13 +23,13 @@ public class Player {
 			for (int i = 0; i < turns; i++) {
 				System.out.println("What would you like to do, Player "+playerNum+"? Type usecard to use a card, showhand to see your hand, and endturn to draw and end your turn.");
 				if(input.nextLine().equalsIgnoreCase("usecard")) {
-					Main.player.useCard();
+					useCard();
 				}
 				if(input.nextLine().equalsIgnoreCase("showhand")) {
-					Main.player.showHand();
+					showHand();
 				}
 				if(input.nextLine().equalsIgnoreCase("endturn")) {
-					Main.player.endTurn();
+					endTurn();
 				}
 			}
 		}
@@ -45,39 +44,37 @@ public class Player {
 		}
 	}
 	public void useCard() {
-		boolean cardInHand = false;
-		while (!cardInHand) {
-			System.out.println("What card would you like to use?");
-			Card playedCard = input.nextLine();
-			if (hand.contains(playedCard)) {
-				System.out.println("Playing "+playedCard+" .");
-				hand.remove(playedCard);
-				Main.deck.discard(playedCard);
-				cardInHand = true;
-			} else {
-				System.out.println("You do not have "+playedCard+" in your hand.");
-			}
-		}
+		System.out.println("What card would you like to use?");
+		Card chosenCard = input.nextLine();
+		if (hand.contains(chosenCard)) {
+			System.out.println("Playing "+chosenCard+" .");
+			hand.remove(chosenCard);
+			Main.deck.discard(chosenCard);
+		} else {
+			System.out.println("You do not have "+playedCard+" in your hand.");
+		}	
 	}
-	public void favor() {
-		boolean cardInHand = false;
-		while (!cardInHand) {
-			System.out.println("What card would you like to give? Type the card name to give or nope to counter the favor.");
-			Card chosenCard = input.nextLine();
-			if (input.nextLine().equalsIgnoreCase("nope") && hand.contains(chosenCard)) {
-				System.out.println("Favor countered");
-				hand.remove(chosenCard);
-				Main.deck.discard(chosenCard);
-				cardInHand = true;
-			} else if (hand.contains(chosenCard)) {
-				System.out.println("Giving "+chosenCard+" .");
-				hand.remove(chosenCard);
-				Main.deck.discard(chosenCard);
-				cardInHand = true;
-			} else {
-				System.out.println("You do not have "+chosenCard+" in your hand.");
-			}
-			
-		}
+	public Card choseCard() {
+		System.out.println("What card would you like to chose?");
+		Card chosenCard = convertToCardType(input.nextLine());
+		if 
+	}
+//	public void favor() {
+//		System.out.println("What card would you like to give? Type the card name to give or nope to counter the favor.");
+//		Card chosenCard = input.nextLine();
+//		if (input.nextLine().equalsIgnoreCase("nope") && isCardInHand(chosenCard)) {
+//			System.out.println("Favor countered");
+//			hand.remove(chosenCard);
+//			Main.deck.discard(chosenCard);
+//		} else if (hand.contains(chosenCard)) {
+//			System.out.println("Giving "+chosenCard+" .");
+//			hand.remove(chosenCard);
+//			Main.deck.discard(chosenCard);
+//		} else {
+//			System.out.println("You do not have "+chosenCard+" in your hand.");
+//		}
+//	}
+	public boolean isCardInHand(Card chosenCard) {
+		return hand.contains(chosenCard);
 	}
 }
