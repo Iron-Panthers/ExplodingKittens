7dc87286121dc13b9c54d276d37a76f0e52d1cc2
Ivan Ke <21key47@mybhs.org>
2017-10-10 19:20:10 -0700

    Fixed Stuff

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -20,13 +20,19 @@ public class Player {
 	public void turn() {
 		for (int i = 0; i < turns; i++) {
 			System.out.print("What would you like to do, "+playerNum+"? Type usecard to use a card, showhand to see your hand, and endturn to draw and end your turn.");
-			if (input.nextLine().equalsIgnoreCase("usecard")) {
-				
+			if(input.nextLine().equalsIgnoreCase("usecard")) {
+				Main.player.useCard();
+			}
+			if(input.nextLine().equalsIgnoreCase("showhand")) {
+				Main.player.showHand();
+			}
+			if(input.nextLine().equalsIgnoreCase("endturn")) {
+				Main.player.endTurn();
 			}
 		}
 	}
-	public void endTurn(Card topCard) {
-		hand.add(topCard);
+	public void endTurn() {
+		hand.add(Main.deck.topCard());
 		if (playerNum < numPlayers.size()) {
 			playerNum++;
 		} else {
@@ -43,21 +49,37 @@ public class Player {
 		boolean cardInHand = false;
 		while (!cardInHand) {
 			System.out.print("What card would you like to use?");
-			String playedCard = input.nextLine();
+			Card playedCard = input.nextLine();
 			if (hand.contains(playedCard)) {
 				cardInHand = true;
 				System.out.print("Playing "+playedCard+" .");
 				hand.remove(playedCard);
+				Main.deck.discard(playedCard);
 			} else {
 				System.out.print("You do not have "+playedCard+" in your hand.");
 			}
 		}
 	}
 	public void favor() {
-		
-	}
-	public void playerNope() {
-		
+		boolean cardInHand = false;
+		while (!cardInHand) {
+			System.out.print("What card would you like to give? Type the card name to give or nope to counter the favor.");
+			Card chosenCard = input.nextLine();
+			if (input.nextLine().equalsIgnoreCase("nope") && hand.contains(chosenCard)) {
+				System.out.print("Favor countered");
+				hand.remove(chosenCard);
+				Main.deck.discard(chosenCard);
+				cardInHand = true;
+			} else if (hand.contains(chosenCard)) {
+				cardInHand = true;
+				System.out.print("Giving "+chosenCard+" .");
+				hand.remove(chosenCard);
+				Main.deck.discard(chosenCard);
+			} else {
+				System.out.print("You do not have "+chosenCard+" in your hand.");
+			}
+			
+		}
 	}
 	public void explodes() {
 		numPlayers.remove(numPlayers.get(playerNum));
