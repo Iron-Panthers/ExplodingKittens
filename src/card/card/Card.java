72adec5a228c0a49643d4dd086e87d76d2f85cf3
Shiloh Andersson <shilohjacee@hotmail.com>
2017-10-11 16:50:33 -0700

    Add convert string to card type method

--- src/card/card/Card.java
+++ src/card/card/Card.java
@@ -1,5 +1,20 @@
 package card;
 
 public class Card {
-
+	
+	public CardType type;
+	
+	public Card(CardType type) {
+		this.type = type;
+		
+	}
+	
+	public CardType convert(String card) {
+		for (int = 0; i < CardType.values().length; i++) {
+			if (CardType.valueof(card.toUpperCase()) == CardType.values()[i]) {
+				return CardType.values()[i];
+			}
+		}
+	}
+	
 }
--- src/card/card/CardType.java
+++ src/card/card/CardType.java
@@ -1,4 +1,9 @@
-
 public enum CardType {
-
+	SKIP,
+	ATTACK,
+	SHUFFLE,
+	FAVOR,
+	NOPE,
+	TWO_OF_A_KIND,
+	THREE_OF_A_KIND
 }
