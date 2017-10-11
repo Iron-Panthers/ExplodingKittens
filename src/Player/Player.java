bf1480bc38f27bcbb5aa23179a4551938975a30a
Ivan Ke <21key47@mybhs.org>
2017-10-10 19:40:46 -0700

    Added a comment

--- src/Player/Player.java
+++ src/Player/Player.java
@@ -3,23 +3,24 @@ package Player;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-
+//make each plaer's hand separate. Construct 4 separate hands. Distinguish between the player who is currently playing.
 public class Player {
 	
 	public Scanner input = new Scanner(System.in);
-	public ArrayList<String> numPlayers;
-	public int playerNum;
+	public ArrayList<String> players;
+	public int playerNum = 1;
 	public int turns = 1;
 	public ArrayList<Card> hand;
 	public boolean hasCard;
 	public Player() {
 		hand = new ArrayList<Card>();
-		this.numPlayers = new ArrayList<String>();
+		players = new ArrayList<String>();
 	}
 	
 	public void turn() {
+		turns = 1;
 		for (int i = 0; i < turns; i++) {
-			System.out.print("What would you like to do, "+playerNum+"? Type usecard to use a card, showhand to see your hand, and endturn to draw and end your turn.");
+			System.out.println("What would you like to do, Player "+playerNum+"? Type usecard to use a card, showhand to see your hand, and endturn to draw and end your turn.");
 			if(input.nextLine().equalsIgnoreCase("usecard")) {
 				Main.player.useCard();
 			}
@@ -33,7 +34,7 @@ public class Player {
 	}
 	public void endTurn() {
 		hand.add(Main.deck.topCard());
-		if (playerNum < numPlayers.size()) {
+		if (playerNum < players.size()) {
 			playerNum++;
 		} else {
 			playerNum = 1;
@@ -42,46 +43,46 @@ public class Player {
 	}
 	public void showHand() {
 		for (int i = 0; i < hand.size(); i++) {
-			System.out.print(hand.get(i));
+			System.out.println(hand.get(i));
 		}
 	}
 	public void useCard() {
 		boolean cardInHand = false;
 		while (!cardInHand) {
-			System.out.print("What card would you like to use?");
+			System.out.println("What card would you like to use?");
 			Card playedCard = input.nextLine();
 			if (hand.contains(playedCard)) {
-				cardInHand = true;
-				System.out.print("Playing "+playedCard+" .");
+				System.out.println("Playing "+playedCard+" .");
 				hand.remove(playedCard);
 				Main.deck.discard(playedCard);
+				cardInHand = true;
 			} else {
-				System.out.print("You do not have "+playedCard+" in your hand.");
+				System.out.println("You do not have "+playedCard+" in your hand.");
 			}
 		}
 	}
 	public void favor() {
 		boolean cardInHand = false;
 		while (!cardInHand) {
-			System.out.print("What card would you like to give? Type the card name to give or nope to counter the favor.");
+			System.out.println("What card would you like to give? Type the card name to give or nope to counter the favor.");
 			Card chosenCard = input.nextLine();
 			if (input.nextLine().equalsIgnoreCase("nope") && hand.contains(chosenCard)) {
-				System.out.print("Favor countered");
+				System.out.println("Favor countered");
 				hand.remove(chosenCard);
 				Main.deck.discard(chosenCard);
 				cardInHand = true;
 			} else if (hand.contains(chosenCard)) {
-				cardInHand = true;
-				System.out.print("Giving "+chosenCard+" .");
+				System.out.println("Giving "+chosenCard+" .");
 				hand.remove(chosenCard);
 				Main.deck.discard(chosenCard);
+				cardInHand = true;
 			} else {
-				System.out.print("You do not have "+chosenCard+" in your hand.");
+				System.out.println("You do not have "+chosenCard+" in your hand.");
 			}
 			
 		}
 	}
 	public void explodes() {
-		numPlayers.remove(numPlayers.get(playerNum));
+		players.remove(players.get(playerNum));
 	}
 }
