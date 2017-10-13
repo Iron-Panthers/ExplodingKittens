819374e0874bb1f4a90fb30c61b911329a70729b
Unknown <james@varah.com>
2017-10-13 15:35:38 -0700

    Added Main method, constructed things

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,5 +1,12 @@
 package Main;
 
-public class Main {
+import Deck.Deck;
 
+public class Main{
+	static int numPlayers = 4;
+	static int explodingKittenNum = numPlayers-1;
+	static Deck deck;
+	public static void main(String[] args) {
+		deck = new Deck(explodingKittenNum);
+	}
 }
