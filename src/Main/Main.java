86dff93640389012ddadd64b4ee2bb79fdf8ef7c
Ivan Ke <21key47@mybhs.org>
2017-10-13 15:46:50 -0700

    Added while loop

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -6,7 +6,11 @@ public class Main{
 	static int numPlayers = 4;
 	static int explodingKittenNum = numPlayers-1;
 	static Deck deck;
+	static int playersAlive = numPlayers;
 	public static void main(String[] args) {
 		deck = new Deck(explodingKittenNum);
+		while (playersAlive > 1) {
+			
+		}
 	}
 }
