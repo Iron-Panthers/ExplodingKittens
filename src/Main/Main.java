241a60c7ef3ea311491e0993f36982986cba0e25
Unknown <james@varah.com>
2017-10-13 16:14:24 -0700

    Added partial exploding kitten method

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,7 +1,10 @@
 package Main;
 
-import Deck.Deck;
+import java.util.ArrayList;
+import java.util.Scanner;
+
 import CardPackage.*;
+import Deck.Deck;
 import Player.*;
 
 public class Main{
@@ -9,11 +12,16 @@ public class Main{
 	static int explodingKittenNum = numPlayers-1;
 	static Deck deck;
 	static int playersAlive = numPlayers;
+	static ArrayList<Player> players;
+	static Scanner input;
 	public static void main(String[] args) {
+		input = new Scanner(System.in);
 		deck = new Deck(explodingKittenNum);
+		players = new ArrayList<Player>();
 		for (int i = 0; i<numPlayers; i++) {
 			String playerName = "player"+i;
 			static Player playerName = new Player();
+			players.add(playerName);
 		}
 		while (playersAlive > 1) {
 			
@@ -22,15 +30,33 @@ public class Main{
 	public void shuffle() {
 		deck.shuffle();
 	}
-	public void skip() {
+	public void skip(Player skipper) {
 		//Does Stuff
 		//Ends turn, no draw
 	}
-	public void attack() {
+	public void attack(Player attacker) {
 		//Ends turn, next player must take two turns
 	}
-	public void favor() {
+	public void favor(Player targeter, Player victim) {
+		//Targets a player, who chooses a card and gives it back to the targeter
+	}
+	public void nope(Player noper, Player victim) {
+		//Targets a card that targets the noper
+	}
+	public void defuse(Player defuser) {
 		
 	}
-	
+	public void explode(Player exploder) { //NOT FINISHED
+		System.out.println("You have drawn an exploding kitten. Type 'defuse' to defuse, type 'ok' to die");
+		String usrInput = input.nextLine();
+		if (usrInput.equalsIgnoreCase("defuse")) {
+			if (exploder.hand.contains(Card defuse)) {
+				System.out.println("Where would you like to place the exploding kitten?");
+				//System.out.println("You can place the exploding kitten anywhere in the deck from the top, 0, to "+deck.deckList.size()-1);
+			}
+		}
+	}
+	public void draw(Player drawer) {
+		drawer.hand.add(deck.topCard());
+	}
 }
