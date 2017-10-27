2910989ff25d3ab9dd1a04629715a3d5ac640cfe
Unknown <james@varah.com>
2017-10-27 15:50:24 -0700

    Updated Skip Method

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -258,8 +258,9 @@ public class Main{
 	public static void endTurnNoDraw() { //Ends turn but places back card player would have drawn normally in the endTurn method. Possibly needs to be added in the 0 location in the deck
 		Player skipper = players.get(currentPlayer);
 		skipper.endTurn();
-		deck.deckList.add(0,getDrawnCard());  
-		skipper.hand.remove(getDrawnCard());
+		Card tempDrawnCard = getDrawnCard();
+		deck.deckList.add(0,tempDrawnCard);  
+		skipper.hand.remove(tempDrawnCard);
 	}
 	public static void twoOfAKindSteal() {
 		Player targeter = players.get(currentPlayer);
